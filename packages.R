d8769ef75039eebc3d8cdb1dfd18538131cf1a21
Maxwel Coura Oliveira <maxwelco@gmail.com>
2021-07-01 15:13:25 -0300

    mnm

---
+++ packages.R
@@ -0,0 +1,12 @@
+/Users/maxwelco/Documents
+
+
+library(xaringan)
+library(xaringanthemer)
+library(xaringanExtra)
+library(metathis)
+
+usethis::use_course(
+  url = "presentable-user2021/materials",
+  destdir = "/Users/maxwelco/Documents/xaringan_workshop/"
+)
